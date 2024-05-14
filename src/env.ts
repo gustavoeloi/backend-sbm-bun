f55d375bf5da861578f94c9f5ce37ca8667718ef
gustavooeloi <gustavooeloi@gmail.com>
2024-05-14 15:01:40 -0300

    feat: setup jwt

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const envSchema = z.object({
   DATABASE_URL: z.string().url().min(1),
   AUTH_REDIRECT_URL: z.string().url().min(1),
+  JWT_SECRET_KEY: z.string().min(1),
   API_BASE_URL: z.string().url().min(1),
 });
 
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,23 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { registerEstablishment } from "./routes/register-establishment";
 import { sendAuthLink } from "./routes/send-auth-link";
+import jwt from "@elysiajs/jwt";
+import { env } from "../env";
+import cookie from "@elysiajs/cookie";
 
-const app = new Elysia().use(registerEstablishment).use(sendAuthLink);
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        establishmentsId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(registerEstablishment)
+  .use(sendAuthLink);
 
 app.listen(3333, () => {
   console.log("Http Server is Running...");
