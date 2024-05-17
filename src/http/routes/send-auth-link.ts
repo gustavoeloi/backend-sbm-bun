8fd443d5ca7698498b7ff74f8528629ddc66e352
gustavooeloi <gustavooeloi@gmail.com>
2024-05-17 11:30:22 -0300

    feat: nodemailer (email test)

--- src/http/routes/send-auth-link.ts
+++ src/http/routes/send-auth-link.ts
@@ -3,6 +3,9 @@ import { db } from "../../db/connection";
 import { createId } from "@paralleldrive/cuid2";
 import { authLinks } from "../../db/schema";
 import { env } from "../../env";
+import { mail } from "../lib/nodemailer";
+
+import nodemailer from 'nodemailer'
 
 export const sendAuthLink = new Elysia().post(
   "/authenticate",
@@ -26,14 +29,24 @@ export const sendAuthLink = new Elysia().post(
       code: authLinkCode,
     });
 
-    // TODO: Enviar E-mail
-
     const authLink = new URL("/auth-links/authenticate", env.API_BASE_URL);
 
     authLink.searchParams.set("code", authLinkCode);
     authLink.searchParams.set("redirect", env.AUTH_REDIRECT_URL);
 
     console.log(authLink.toString());
+
+    // TODO: Enviar E-mail
+    const emailLink = await mail.sendMail({
+      from: {
+        name: "SBM - Small Bussiness Managment",
+        address: "dev@sbm.com"
+      },
+      to: email,
+      subject: "Link de autenticação ao SBM",
+      text: `Clique no seguinte link para se autenticar ao SBM ${authLink.toString()}`
+    })
+    console.log(nodemailer.getTestMessageUrl(emailLink))
   },
   {
     body: t.Object({
