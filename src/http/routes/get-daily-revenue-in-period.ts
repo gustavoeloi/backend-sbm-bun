6da39e5b1fbc1ae18cedd89ce718eeddbb9002f5
gustavooeloi <gustavooeloi@gmail.com>
2024-05-27 21:02:58 -0300

    feat: getting daily revenue in period

---
+++ src/http/routes/get-daily-revenue-in-period.ts
@@ -0,0 +1,54 @@
+import Elysia, { t } from "elysia";
+import { auth } from "../auth";
+import dayjs from "dayjs";
+import { UnauthorizedError } from "../errors/unauthorized-errors";
+import { db } from "../../db/connection";
+import { and, desc, eq, gte, lte, sql, sum } from "drizzle-orm";
+import { orders } from "../../db/schema";
+
+export const getDailyRevenueInPeriod = new Elysia().use(auth).get(
+  "/metrics/daily-revenue-in-period",
+  async ({ cookie: { authCookie }, jwt, query: { from, to }, set }) => {
+    const cookie = String(authCookie.cookie.value);
+
+    const payload = await jwt.verify(cookie);
+
+    if (!payload || !payload.establishmentsId) {
+      throw new UnauthorizedError();
+    }
+
+    const startDate = from ? dayjs(from) : dayjs().subtract(7, "days");
+    const endDate = to ? dayjs(to) : from ? startDate.add(7, "days") : dayjs();
+
+    if (endDate.diff(startDate, "days") > 7) {
+      set.status = 400;
+
+      return {
+        message: "You cannot list revenue in a larger period than 7 days",
+      };
+    }
+
+    const revenuePerDay = await db
+      .select({
+        date: sql<string>`TO_CHAR(${orders.createdAt}, 'DD/MM')`,
+        revenue: sum(orders.totalInCents).mapWith(Number),
+      })
+      .from(orders)
+      .where(
+        and(
+          eq(orders.establishmentId, payload.establishmentsId),
+          gte(orders.createdAt, startDate.startOf("day").toDate()),
+          lte(orders.createdAt, endDate.endOf("day").toDate())
+        )
+      )
+      .groupBy(sql`TO_CHAR(${orders.createdAt}, 'DD/MM')`)
+      
+    return revenuePerDay;
+  },
+  {
+    query: t.Object({
+      from: t.Optional(t.String()),
+      to: t.Optional(t.String()),
+    }),
+  }
+);
--- src/http/routes/get-day-orders-amount.ts
+++ src/http/routes/get-day-orders-amount.ts
@@ -6,42 +6,58 @@ import { db } from "../../db/connection";
 import { orders } from "../../db/schema";
 import { and, count, eq, gte, sql } from "drizzle-orm";
 
+export const getDayOrdersAmount = new Elysia()
+  .use(auth)
+  .get(
+    "/metrics/day-orders-amount",
+    async ({ cookie: { authCookie }, jwt }) => {
+      const cookie = String(authCookie.cookie.value);
 
-export const getDayOrdersAmount = new Elysia().use(auth).get('/metrics/day-orders-amount', async ({ cookie: { authCookie }, jwt, }) => {
+      const payload = await jwt.verify(cookie);
 
-  const cookie = String(authCookie.cookie.value);
+      if (!payload || !payload.establishmentsId) {
+        throw new UnauthorizedError();
+      }
 
-  const payload = await jwt.verify(cookie);
+      const today = dayjs();
+      const yesterday = today.subtract(1, "day");
+      const startOfYesterday = yesterday.startOf("day");
 
-  if (!payload || !payload.establishmentsId) {
-    throw new UnauthorizedError()
-  }
+      const orderPerDay = await db
+        .select({
+          dayWithMonthAndYear: sql<string>`TO_CHAR(${orders.createdAt}, 'YYYY-MM-DD')`,
+          amount: count(),
+        })
+        .from(orders)
+        .where(
+          and(
+            eq(orders.establishmentId, payload.establishmentsId),
+            gte(orders.createdAt, startOfYesterday.toDate())
+          )
+        )
+        .groupBy(sql`TO_CHAR(${orders.createdAt}, 'YYYY-MM-DD')`);
 
-  const today = dayjs();
-  const yesterday = today.subtract(1, 'day')
-  const startOfYesterday = yesterday.startOf('day')
+      const todayWithMonthAndYear = today.format("YYYY-MM-DD");
+      const yesterdayWithMonthAndYear = yesterday.format("YYYY-MM-DD");
 
-  const orderPerDay = await db.select({
-    dayWithMonthAndYear: sql<string>`TO_CHAR(${orders.createdAt}, 'YYYY-MM-DD')`,
-    amount: count()
-  }).from(orders).where(and(eq(orders.establishmentId, payload.establishmentsId), gte(orders.createdAt, startOfYesterday.toDate()))).groupBy(sql`TO_CHAR(${orders.createdAt}, 'YYYY-MM-DD')`)
+      const todayOrdersAmount = orderPerDay.find((orderPerDay) => {
+        return orderPerDay.dayWithMonthAndYear === todayWithMonthAndYear;
+      });
 
-  const todayWithMonthAndYear = today.format('YYYY-MM-DD');
-  const yesterdayWithMonthAndYear = yesterday.format('YYYY-MM-DD');
+      const yesterdayOrdersAmount = orderPerDay.find((orderPerDay) => {
+        return orderPerDay.dayWithMonthAndYear === yesterdayWithMonthAndYear;
+      });
 
-  const todayOrdersAmount = orderPerDay.find((orderPerDay) => {
-    return orderPerDay.dayWithMonthAndYear === todayWithMonthAndYear
-  })
+      const diffFromYesterday =
+        todayOrdersAmount && yesterdayOrdersAmount
+          ? (todayOrdersAmount.amount * 100) / yesterdayOrdersAmount?.amount
+          : null;
 
-  const yesterdayOrdersAmount = orderPerDay.find((orderPerDay) => {
-    return orderPerDay.dayWithMonthAndYear === yesterdayWithMonthAndYear
-  })
-
-  const diffFromYesterday = todayOrdersAmount && yesterdayOrdersAmount ? (todayOrdersAmount.amount * 100) / yesterdayOrdersAmount?.amount : null
-
-  return {
-    amount: todayOrdersAmount?.amount || 0,
-    diffFromLastDay: diffFromYesterday ? Number((diffFromYesterday - 100).toFixed(2)) : 0
-  }
-
-})
+      return {
+        amount: todayOrdersAmount?.amount || 0,
+        diffFromLastDay: diffFromYesterday
+          ? Number((diffFromYesterday - 100).toFixed(2))
+          : 0,
+      };
+    }
+  );
